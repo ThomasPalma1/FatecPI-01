39d53bf968ba401d6e821017839dd50342c56bb5
JulianeFreitass <juliane.freitas55@gmail.com>
2020-10-05 11:11:24 -0300

    Linkando as paginas

---
+++ src/carousel/login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include ('conexao.php');
+
+if (empty( $_POST ['email']) || empty ($_POST['password'])) {
+    header ('Location:index.php');
+    exit();
+} 
+
+$email = mysqli_real_escape_string($conexao, $_POST['email']);
+$senha = mysqli_real_escape_string($conexao,$_POST['password']);
+
+$query = "select email from usuario where email = '$email' and senha = md5 ('$senha')";
+$result = mysqli_query ($conexao, $query);
+$row = mysqli_num_rows ($result);
+
+if ($row ==1){
+    $_SESSION['email'] = $email;
+    $_SESSION['senha'] = $senha;
+    header ('Location: produtos.html');
+    exit ();
+}
+else {  
+    header ('Location: logar.php');
+    exit ();
+}
