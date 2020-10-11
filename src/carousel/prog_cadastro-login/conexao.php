1dad9ab0d44676724ce1851f40c0f07fd5a58ab5
JulianeFreitass <juliane.freitas55@gmail.com>
2020-10-11 14:02:30 -0300

    adc perfil do adm

--- src/carousel/prog_cadastro-login/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST','127.0.0.1');
-define('USUARIO','root');
-define('SENHA','');
-define('BD','bd_pi01');
-
-$conexao = mysqli_connect (HOST,USUARIO,SENHA,BD) or die ('Could not connect');
